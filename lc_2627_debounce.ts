be1365630d3152c670868d56aa666a097179438c
pawas <pawas.saxena+1@gmail.com>
2023-12-05 18:42:04 +0530

    debounce

---
+++ lc_2627_debounce.ts
@@ -0,0 +1,31 @@
+type F = (...args: number[]) => void;
+
+function debounce(fn: F, t: number): F {
+  let lastCalledTime = -Infinity;
+  let lastTimeoutId = undefined;
+
+  return function (...args) {
+    const currentTime = new Date().getTime();
+    if (currentTime - lastCalledTime >= t) {
+      lastCalledTime = currentTime;
+
+      createTimeout();
+    } else {
+      // move timeout ahead
+      clear();
+      createTimeout();
+    }
+
+    function clear() {
+      lastTimeoutId && clearTimeout(lastTimeoutId);
+      lastCalledTime = new Date().getTime();
+    }
+
+    function createTimeout() {
+      lastTimeoutId = setTimeout(() => {
+        lastCalledTime = new Date().getTime();
+        fn(...args);
+      }, t);
+    }
+  };
+}
