b1a0157bd2385c1c045f7438cb3df2b592d7fa54
pawas <pawas.saxena+1@gmail.com>
2023-12-09 19:40:02 +0530

    promise pool my soln

---
+++ src/lc_2636_promisePool.ts
@@ -0,0 +1,48 @@
+// https://youtu.be/DB8pAAg-9xw?si=TdRVvtkHyayAJJLe
+export function promisePool(fns: Promise<any>[], n: number): Promise<any> {
+  return new Promise(async (resolve, reject) => {
+    const nestedPromises = nest(fns, n);
+    let successResults = [];
+    for (let i = 0; i < nestedPromises.length; i++) {
+      const promisePoolChunk = nestedPromises[i];
+      try {
+        const chunkSuccessResult = await Promise.all(promisePoolChunk);
+        successResults = [...successResults, ...chunkSuccessResult];
+      } catch (err) {
+        return reject(err);
+      }
+    }
+
+    resolve(successResults);
+  });
+}
+
+export function nest(arr: any[], subArrLen: number): any[][] {
+  const result: any[][] = [];
+
+  const nested: any[][] = arr.reduce((prev, curr) => {
+    prev = addToLastArr(prev, subArrLen, curr);
+    return prev;
+  }, result);
+
+  return nested;
+}
+
+function addToLastArr(
+  arr: any[][],
+  desiredInnerSize: number,
+  toAdd: any,
+): any[] {
+  if (arr.length === 0) {
+    arr.push([toAdd]);
+  } else {
+    const lastElementLength = arr[arr.length - 1].length;
+    if (lastElementLength === desiredInnerSize) {
+      arr.push([toAdd]);
+    } else {
+      arr[arr.length - 1].push(toAdd);
+    }
+  }
+
+  return arr;
+}
